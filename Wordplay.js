dc64fbfa957dc343514f7636110118ded45675aa
Jason <90075907+Jason-Doze@users.noreply.github.com>
2022-04-19 17:30:16 -0500

    adding questions and txt file

---
+++ Wordplay.js
@@ -0,0 +1,25 @@
+var txt;
+
+function preload() {
+    text = loadStrings('sowpods.txt');
+}
+
+// What are all of the words containing UU?
+// What are all of the words containing an X and a Y and a Z?
+// What are all of the words containing a Q but not a U?
+// What are all of the words that contain the word CAT and are exactly 5 letters long?
+// What are all of the words that have no E or A and are at least 15 letters long?
+// What are all of the words that have a B and an X and are less than 5 letters long?
+// What are all of the words that both start and end with a Y?
+// What are all of the words with no vowel and not even a Y?
+// What are all of the words that have all 5 vowels, in any order?
+// What are all of the words that have all 5 vowels, in alphabetical order?
+
+// Setting up storage to use during a for loop, including counters and arrays
+// How many words contain the substring "TYPE”?
+// Create and print an array containing all of the words that end in "GHTLY"
+// What is the shortest word that contains all 5 vowels?
+// What is the longest word that contains no vowels?
+// Which of the letters Q, X, and Z is the least common?
+// What is the longest palindrome?
+// What are all of the letters that never appear consecutively in an English word? For example, we know that “U” isn’t an answer, because of the word VACUUM, and we know that “A” isn’t an answer, because of “AARDVARK”, but which letters never appear consecutively?
