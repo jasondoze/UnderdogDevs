f72438b29714e201ca96b78b10192b195015321c
Jason <90075907+Jason-Doze@users.noreply.github.com>
2022-06-12 17:59:30 -0500

    add countries and notes to repo

---
+++ countries/nameWithinName.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const { Console } = require("console");
+const { captureRejections } = require("events");
+const fs = require("fs");
+const { report } = require("process");
+const { resourceLimits } = require("worker_threads");
+const text = fs.readFileSync("countries.txt");
+const textByLine = text.toString().toLowerCase().split("\n");
+
+//  There is at least one country name that contains another country name. Find all of these cases.
+
+function countryWithinCountry(input) {
+
+}
+
+console.log(countryWithinCountry(textByLine));
