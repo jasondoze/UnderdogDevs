f8e2dd5d8a7c45e83f9b03496d197645e8991ce6
Jason <90075907+Jason-Doze@users.noreply.github.com>
2022-06-12 20:54:57 -0500

    add solutions to challenges

--- countries/oneVowel.js
+++ countries/oneVowel.js
@@ -1,33 +1,27 @@
-
 const fs = require("fs");
-const text = fs.readFileSync("curlCountries.txt", {
+const text = fs.readFileSync("countries/curlCountries.txt", {
   encoding: "utf8",
   flag: "r",
 });
 
 // What countries use only one vowel in their name (the vowel can be used multiple times)
 // For example, if the word "BEEKEEPER" were a country, it would be an answer, because it only uses "E".
-
-// for (const country of countryArr) {
-//   let firstVowel = undefined;
-//   let hasOneVowel = false;
-
-//   for (const letter of country) {
-//     if (vowels.includes(letter)) {
-//       if (!firstVowel) {
-//         firstVowel = letter;
-//         hasOneVowel = true;
-//       } else if (letter !== firstVowel) {
-//         hasOneVowel = false;
-//         continue;
-//       }
-//     }
-//   }
-
-//   if (hasOneVowel) {
-//     countryHasSingleVowel.push(country);
-//   }
-// }
-// console.log({ countryHasSingleVowel });
-
+function oneVowel(input) {
+  let countries = input.split("\n");
+  let result = [];
+  for (let i = 0; i < countries.length; i++) {
+    let country = countries[i];
+    let vowels = country.match(/[aeiou]/gi);
+    // if the country has multiple vowels and all the vowels are the same letter, push it to the result array
+    if (
+      vowels &&
+      vowels.length > 1 &&
+      vowels.every((vowel) => vowel === vowels[0])
+    ) {
+      result.push(country);
+    }
+  }
+  return result;
+}
+console.log(oneVowel(text));
 
