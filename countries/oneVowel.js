87545329c1eb13f8a7d90e49aeb6d9ac46dcf8de
Jason <90075907+Jason-Doze@users.noreply.github.com>
2022-06-20 19:33:29 -0500

    add challenges

--- countries/oneVowel.js
+++ countries/oneVowel.js
@@ -4,20 +4,20 @@ const { captureRejections } = require("events");
 const fs = require("fs");
 const { report } = require("process");
 const { resourceLimits } = require("worker_threads");
-const text = fs.readFileSync(
-  "/Volumes/My Passport for Mac - Data/UnderDog_Devs/UnderdogDevs/countries/countries.txt"
-);
-const textByLine = text.toString().toLowerCase();
+const text = fs.readFileSync("countries.txt");
+const textByLine = text.toString().toLowerCase().split("\n");
 
 // What countries use only one vowel in their name (the vowel can be used multiple times)
 // For example, if the word "BEEKEEPER" were a country, it would be an answer, because it only uses "E".
 
-function oneVowel(text) {
-  let countries = text.split("\n");
+function oneVowel(countries) {
   let oneVowelCountries = [];
+
+  // iterate over the array, assign country to each element
   for (let i = 0; i < countries.length; i++) {
     let country = countries[i];
     console.log(country);
+
     let vowels = country.match(/[aeiou]/gi);
     // The every() method tests whether all elements in the array pass the test
     // implemented by the provided function. It returns a Boolean value.
@@ -27,4 +27,10 @@ function oneVowel(text) {
   }
   return oneVowelCountries;
 }
-console.log(oneVowel(textByLine));
+
+// const lowerCaseText = text.toString().toLowerCase();
+// // const splitBy = "\n"; // Jason to put the right value here
+// const splitBy = "a"; // Jason to put the right value here
+
+// const allCountries = lowerCaseText.split(splitBy);
+// console.log(oneVowel(allCountries));
