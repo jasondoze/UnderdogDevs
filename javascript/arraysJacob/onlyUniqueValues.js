ce1156f8c30d37626e37e552a8cb1bb552371c66
Jason Doze <jdoze@protonmail.com>
2022-09-17 20:08:47 -0500

    Add onlyUnique bug fix.
    Signed-off-by: Jason Doze <jdoze@protonmail.com>

--- javascript/arraysJacob/onlyUniqueValues.js
+++ javascript/arraysJacob/onlyUniqueValues.js
@@ -37,25 +37,175 @@
 
 // console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 5])); // [2, 5]
 // establish a first index element
+// what properties should the
+/*
+strings, bools, etc
+*/
+
+// // console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+// console.log(onlyUnique([])); // []
+// console.log(onlyUnique(['a'])); // ['a']
+// console.log(onlyUnique(['a', 'a'])); // []
+// console.log(onlyUnique(['a', 'a', 'a']));
+
+// spreadsheets, and correct version of this solution
+
+// let onlyUnique = (someArray) => {
+//   let result = [];
+//   let count = {};
+//   l
+//   for (const i of someArray) {
+//     let item = someArray[i];
+//     if (!count[item]) {
+//       count[item] = 1;
+//     } else {
+//       count[item] + 1;
+//     }
+//   }
+
+//   return count;
+// };
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+// let onlyUnique = (someArray) => {
+//   // create a variable called set to create a new set
+//   let set = new Set();
+//   let result = [];
+//   // iterate over the someArray
+//   // if the set has any duplicate elements in the array, delete them
+//   for (let i = 0; i < someArray.length; i++) {
+//     console.log(someArray[i], someArray.indexOf(someArray[i]));
+//     if (
+//       someArray.indexOf(someArray[i]) !== someArray.indexOf(someArray[i + 1])
+//     ) {
+//       result.push(someArray[i]);
+//     }
+//   }
+
+// the set has the number already
+// if (!set.has(i)) {
+// then delete the number
+// without delete, maybe a set and an array
+// set.add(i);
+// console.log(set);
+// otherwise add them the set
+// } else {
+//   set.add(i);
+//   // console.log(set);
+// }
+
+// return the set with a spread operator in an array to match expected output
+//   return result;
+// };
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+// console.log(onlyUnique([])); // []
+// console.log(onlyUnique(['a'])); // ['a']
+// console.log(onlyUnique(['a', 'a'])); // []
+// console.log(onlyUnique(['a', 'a', 'a'])); // []
+
+// let onlyUnique = (someArray) => {
+//   let filtered = someArray.filter((value, index, arr) => {
+//     return arr.indexOf(value) === index;
+//   });
+//   return filtered;
+// };
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+
+// let onlyUnique = (someArray) => {
+//   let result = [];
+//   for (let i = 0; i < someArray.length; i++) {
+//     for (let j = i + 1; j < someArray.length; j++) {
+//       console.log(someArray[i], someArray[j]);
+//       if (someArray[i] !== someArray[j]) {
+//       //   // console.log(someArray.splice(0, i + j))
+//         someArray.splice(someArray[i], someArray[j])
+
+//       }
+//     }
+//   }
+//   return someArray;
+// };
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+
+// let onlyUnique = (someArray) => {
+//   let set = new Set();
+//   for (let i = 0; i < someArray.length; i++) {
+//     for (let j = i + 1; j < someArray.length; j++) {
+//       if (someArray.indexOf(someArray[i]) != someArray.indexOf(someArray[j])) {
+//         set.add(someArray[j]);
+//       }
+//     }
+//   }
+//   return set;
+// };
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+
+// let onlyUnique = (someArray) => {
+//   someArray.reduce((previous, current) => {
+//     if (previous === current) {
+//       someArray.splice(current, 1);
+//     }
+//   });
+//   return someArray;
+// };
+
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+
+// let onlyUnique = (someArray) => {
+//   someArray.map((value, index) => {
+//     console.log('element', value);
+//     console.log('index', index);
+//     // console.log();
+//   });
+// };
+
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+
+// let onlyUnique = (someArray) => {
+//   for (let i = 0; i < someArray.length; i++) {
+//     let firstElement = someArray[i];
+
+//     let nextElement = someArray[i + 1];
+//     console.log('first', firstElement, 'next', nextElement);
+//     if (firstElement === nextElement) {
+//       someArray.splice(i, 1);
+//       // firstElement = nextElement;
+//     }
+//   }
+//   return someArray;
+// };
+
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
 
 let onlyUnique = (someArray) => {
-  // create a variable calles set to creat a new set
-  let set = new Set();
-  // iterate over the someArray
-  // if the set has any duplicate elements in the array, delete them
-  for (let i of someArray) {
-    // the set has the number already
-    if (set.has(i)) {
-      // then delete the number
-      set.delete(i);
-      console.log(set);
-      // otherwise add them the set
-    } else {
-      set.add(i);
-      console.log(set);
-    }
-  }
-  // return the set with a spread operator in an array to match expected output
-  return [...set];
+  const unique = someArray.filter((value) => {
+    console.log(
+      '   indexOf',
+      someArray.indexOf(value),
+      '   lastIndexOf',
+      someArray.lastIndexOf(value)
+    );
+
+    return someArray.indexOf(value) === someArray.lastIndexOf(value);
+  });
+  return unique;
 };
-console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 5])); // [2, 5]
+
+console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+
+// let onlyUnique = (someArray) => {
+//   let result = [];
+//   for (let i = 0; i < someArray.length; i++) {
+//     if (
+//       someArray.indexOf(someArray[i]) === someArray.lastIndexOf(someArray[i])
+//     ) {
+//       result.push(someArray[i]);
+//     }
+//   }
+//   return result;
+// };
+
+// console.log(onlyUnique([1, 1, 2, 3, 3, 4, 4, 4, 5])); // [2, 5]
+// console.log(onlyUnique([])); // []
+// console.log(onlyUnique(['a'])); // ['a']
+// console.log(onlyUnique(['a', 'a'])); // []
+// console.log(onlyUnique(['a', 'a', 'a'])); // []
--- javascript/arraysJacob/orderBySum.js
+++ javascript/arraysJacob/orderBySum.js
@@ -1,11 +1,33 @@
 // Order the arrays by sum, ascending or descending
-// Given an array of arrays (ie. a multidimensional array), 
-// each containing only numeric values, order these arrays 
+// Given an array of arrays (ie. a multidimensional array),
+// each containing only numeric values, order these arrays
 // by the sum of the elements within.
 
 const orderBySum = (someArray) => {
-    // your code
-}
+  // create a sum variable to add the array elements
+  let reducedPairs = [];
+  // iterate over the array of arrays
+  for (let i = 0; i < someArray.length; i++) {
+    // create a pairs array variable
+    let pairs = someArray[i];
+    // console.log(pairs);
+    // add up the pairs of numbers
+    let sumPairs = pairs.reduce((previous, current) => {
+      return previous + current;
+    }, 0);
+    reducedPairs.push(sumPairs);
+    let sortedPairs = reducedPairs.sort((a, b) => {
+      return a - b;
+    });
+    console.log(sortedPairs, sumPairs);
+  }
+};
 
-console.log(orderBySum([[1,3], [4,2], [2,1]]))
-// [[2,1], [1,3], [4,2]]
+console.log(
+  orderBySum([
+    [1, 3],
+    [4, 2],
+    [2, 1],
+  ])
+);
+// // [[2,1], [1,3], [4,2]]
