92a03af063ea4ad1aabea12788cc6e415b9ba132
Jason Doze <dozejason@gmail.com>
2022-12-15 14:34:34 -0600

    Add solutions to repo.
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- MDN/classes/mammalClass.js
+++ MDN/classes/mammalClass.js
@@ -1,51 +1,59 @@
-
 class Mammal {
   constructor(type, breed, color) {
     this.type = type;
     this.breed = breed;
     this.color = color;
   }
-  call() {
-    console.log(`come over here my little ${this.color} ${this.type} broski`);
+  sleep() {
+    console.log(
+      `good night little ${this.color} ${this.type} ${this.breed} zzzzzz`
+    );
   }
 }
 
 // child class
 class Dog extends Mammal {
-  call() {
-    super.call();
-    console.log(`this ${this.color} ${this.type} is irritating`);
+  constructor(name, breed, color, size) {
+    super('dog', breed, color);
+    this.name = name;
+    // this.type = 'wolf';
+    this.size = size;
+    console.log(
+      `New ${this.name} ${this.color} ${this.type} ${this.size} ${this.breed} is created`
+    );
   }
   bark() {
-    console.log(`this ${this.color} ${this.breed} ${this.type} barks alot`);
-  }
-}
-
-class Cat extends Mammal {
-  call() {
-    super.call();
-    console.log(`this ${this.color} ${this.type} is not supposed to bark`);
-  }
-  scratch() {
     console.log(
-      `when the ${this.type} scratches the furniture, the claws get clipped`
+      `this ${this.color} ${this.breed} ${this.type} ${this.size} barks alot-----`
     );
   }
 }
 
+// class Cat extends Mammal {
+//   constructor(type, breed, color, size, meow) {
+//     super();
+//     console.log(`this ${this.color} ${this.type} is not supposed to bark`);
+//   }
+//   scratch() {
+//     console.log(
+//       `when the ${this.type} scratches the furniture, the claws get clipped`
+//     );
+//   }
+// }
+
 // what if I dont want to pass this arguement
-const newDoggo = new Dog('dog', '', 'brown');
-newDoggo.call();
+const newDoggo = new Dog('scoob', 'dog', 'sheppard', 'brown', 'large');
+// newDoggo.call();
 newDoggo.bark();
 
-const doggo = new Dog('dog', 'sheppard', 'black');
-doggo.call();
-doggo.bark();
+// const doggo = new Dog('dog', 'sheppard', 'black');
+// doggo.call();
+// doggo.bark();
 
-const monkey = new Mammal('monkey', 'australiopithicus', "orange");
-monkey.call();
-const dinosaur = new Mammal('velociraptor', 'V. mongoliensis', 'yellow');
+// const monkey = new Mammal('monkey', 'australiopithicus', 'orange');
+// monkey.call();
+// const dinosaur = new Mammal('velociraptor', 'V. mongoliensis', 'yellow');
 
-const alleyKat = new Cat('cat', 'feral', 'white');
-alleyKat.call();
-alleyKat.scratch();
+// const alleyKat = new Cat('cat', 'feral', 'white');
+// alleyKat.call();
+// alleyKat.scratch();
