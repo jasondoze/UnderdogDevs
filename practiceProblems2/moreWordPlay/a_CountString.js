ce3d8d3511ceb23d6024cdf1f015be469c36da9c
Jason Doze <dozejason@gmail.com>
2023-02-03 16:15:50 -0600

    Add solutions to repo.
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- practiceProblems2/moreWordPlay/a_CountString.js
+++ practiceProblems2/moreWordPlay/a_CountString.js
@@ -1,8 +1,4 @@
-const words = require('fs')
-  .readFileSync('../sowpods.txt')
-  .toString()
-  .toLowerCase()
-  .split('\n');
+const words = require('fs').readFileSync('../sowpods.txt').toString().toLowerCase().split('\n');
 
 // Write a function that takes a string `word` as an argument and returns a count of all of the “A”s in that string.
 
@@ -22,3 +18,19 @@ const aCounter = (word) => {
   return obj;
 };
 console.log(aCounter(words));
+
+const countA = (array) => {
+  // reduce the array into an empty obj called wordObj and iterate through rows
+  const wordsObject = array.reduce((wordObj, row) => {
+    // split the rows of words into chars and if any letter is an a, its assigned to the count variables length
+    const count = row.split('').filter((letter) => letter === 'a').length;
+    // if the count is greater than zero
+    if (count > 0) {
+      // wordObject at word = count
+      wordObj[row] = count;
+    }
+    return wordObj;
+  }, {});
+  return wordsObject;
+};
+console.log(countA(words));
